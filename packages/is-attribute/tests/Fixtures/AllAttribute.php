35b8d0d52a73a391ce2117b84a9d0c6cdb6b07d2
Tyler Reed <tylernathanreed@gmail.com>
2023-06-01 14:19:57 -0500

    + Created is-attribute package

---
+++ packages/is-attribute/tests/Fixtures/AllAttribute.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Reedware\IsAttribute\Tests\Fixtures;
+
+use Attribute;
+
+#[Attribute(Attribute::TARGET_ALL)]
+class AllAttribute
+{
+
+}
---
+++ packages/is-attribute/tests/Fixtures/GenericAttribute.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Reedware\IsAttribute\Tests\Fixtures;
+
+use Attribute;
+
+#[Attribute]
+class GenericAttribute
+{
+
+}
